7a383f42f0d7c9669e2c21ec9027f0a10aa1725d
Bernie Zhong <chu23008@byui.edu>
2024-12-11 13:33:02 +0800

    login page update(less css)

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,11 +1,11 @@
 // Needed Resources 
 const express = require("express") // bring Express into scope of file.
 const router = new express.Router()     
-// const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const utilities = require("../utilities")
 
 // Add a "GET" route for the path that will be sent when the "My Account" link is clicked.
 // The "GET" route must use a function from the account controller, to handle the request.
-router.get("/login", accountController.buildLogin)
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,18 +5,23 @@
 /* ***********************
  * Require Statements
  *************************/
-const expressLayouts = require("express-ejs-layouts")
+const inventoryRoute = require("./routes/inventoryRoute")
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
-const inventoryRoute = require("./routes/inventoryRoute")
-// Require the Session package and DB connection
 const session = require("express-session")
 const pool = require('./database/')
-const accountRoute = require('./routes/accountRoute')
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Middleware
@@ -39,32 +44,25 @@ app.use(function(req, res, next){ // app.use is applied and a function is passed
   next()
 })
 
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(express.static("public"))  // 讓 Express 服務靜態檔案
+// app.use(express.static("public"))  // 讓 Express 服務靜態檔案
 // Index route
-app.get("/", utilities.handleErrors(baseController.buildHome)) 
 app.use(static)
+app.get("/", utilities.handleErrors(baseController.buildHome)) 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account routes
+app.use("/account", require("./routes/accountRoute"))
+
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
-app.use("/account", accountRoute)
 
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
@@ -72,7 +70,7 @@ app.use("/account", accountRoute)
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 404){ message = err.message} else if (err.status == 500) {message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
